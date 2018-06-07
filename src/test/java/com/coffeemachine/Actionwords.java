e25b3ead68aeeda1fa59a3d921e1d9a3cac239e2
Vitaliy Manson <mihaylukvv@gmail.com>
2018-06-07 13:36:14 +0300

    first CI test

---
+++ src/test/java/com/coffeemachine/Actionwords.java
@@ -0,0 +1,123 @@
+package com.coffeemachine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+public class Actionwords {
+    public CoffeeMachine sut = new CoffeeMachine();
+    public boolean handleWater = false;
+    public boolean handleBeans = false;
+    public boolean handleGrounds = false;
+
+    public void iStartTheCoffeeMachineUsingLanguageLang() {
+        iStartTheCoffeeMachineUsingLanguageLang("en");
+    }
+
+    public void iStartTheCoffeeMachineUsingLanguageLang(String lang) {
+        sut.start(lang);
+    }
+
+    public void iShutdownTheCoffeeMachine() {
+        sut.stop();
+    }
+
+    public void messageMessageShouldBeDisplayed(String message) {
+        assertEquals(sut.message(), message);
+    }
+
+    public void coffeeShouldBeServed() {
+        assertTrue(sut.coffeeServed);
+    }
+
+    public void coffeeShouldNotBeServed() {
+        assertFalse(sut.coffeeServed);
+    }
+
+    public void iTakeACoffee() {
+        sut.takeCoffee();
+    }
+
+    public void iEmptyTheCoffeeGrounds() {
+        sut.emptyGrounds();
+    }
+
+    public void iFillTheBeansTank() {
+        sut.fillBeans();
+    }
+
+    public void iFillTheWaterTank() {
+        sut.fillTank();
+    }
+
+    public void iTakeCoffeeNumberCoffees(int coffeeNumber) {
+        while ((coffeeNumber > 0)) {
+            iTakeACoffee();
+            coffeeNumber = coffeeNumber - 1;
+
+            if (handleWater) {
+                iFillTheWaterTank();
+            }
+
+            if (handleBeans) {
+                iFillTheBeansTank();
+            }
+
+            if (handleGrounds) {
+                iEmptyTheCoffeeGrounds();
+            }
+        }
+    }
+
+    public void theCoffeeMachineIsStarted() {
+        iStartTheCoffeeMachineUsingLanguageLang();
+    }
+
+    public void iHandleWaterTank() {
+        handleWater = true;
+    }
+
+    public void iHandleBeans() {
+        handleBeans = true;
+    }
+
+    public void iHandleCoffeeGrounds() {
+        handleGrounds = true;
+    }
+
+    public void iHandleEverythingExceptTheWaterTank() {
+        iHandleCoffeeGrounds();
+        iHandleBeans();
+    }
+
+    public void iHandleEverythingExceptTheBeans() {
+        iHandleWaterTank();
+        iHandleCoffeeGrounds();
+    }
+
+    public void iHandleEverythingExceptTheGrounds() {
+        iHandleWaterTank();
+        iHandleBeans();
+    }
+
+    public void displayedMessageIs(String freeText) {
+        messageMessageShouldBeDisplayed(freeText);
+    }
+
+    public void iSwitchToSettingsMode() {
+        sut.showSettings();
+    }
+
+    public void settingsShouldBe(String datatable) {
+        Map<String,String> settings = new HashMap<String, String>();
+        for (String line : datatable.split("\n")) {
+            String[] cells = line.split("\\|");
+            settings.put(cells[1].trim(), cells[2].trim());
+        }
+
+        assertEquals(sut.getSettings(), settings);
+    }
+}
---
+++ src/test/java/com/coffeemachine/LoginPageTest.java
@@ -0,0 +1,23 @@
+package com.coffeemachine;
+
+import junit.framework.TestCase;
+
+public class LoginPageTest extends TestCase {
+    // Tags: login loginPage loginUser Priority:High
+    public Actionwords actionwords = new Actionwords();
+
+    public void testUnsuccessfullyLoggedInUserLeavesEmailPasswordFieldsAreEmpty() {
+    }
+
+    public void testUnsuccessfullyLoggedInUserEntersCorrectEmailBUTWrongPasswordCombination() {
+    }
+
+    public void testUnsuccessfullyLoggedInUserEntersWrongEmailPasswordCombination() {
+    }
+
+    public void testUnsuccessfullyLoggedInUserEntersCorrectEmailWithoutSpecifyingSymbolAndDomainAndCorrectPassword() {
+    }
+
+    public void testUnsuccessfullyLoggedInUserEntersCorrectEmailWithSpecifyingSymbolAndDomainAndCorrectPassword() {
+    }
+}
